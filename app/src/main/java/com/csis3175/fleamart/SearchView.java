b1b0484b5d42308527cfbc7c058a263ae92553fa
Kareem Katiya <130093787+HoneyB2020@users.noreply.github.com>
2024-03-14 23:43:53 -0700

    test bottom bar added
    need to scrap top menu. can't make it transparent

--- app/src/main/java/com/csis3175/fleamart/SearchView.java
+++ app/src/main/java/com/csis3175/fleamart/SearchView.java
@@ -1,15 +1,23 @@
 package com.csis3175.fleamart;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchView extends AppCompatActivity {
+public class SearchView extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
+    private BottomNavigationView bottomNavigationView;
 
 
     @Override
@@ -20,6 +28,26 @@ public class SearchView extends AppCompatActivity {
         rView.setLayoutManager(new GridLayoutManager(this, 2));
         CardAdapter cardAdapter = new CardAdapter(getCardData());
         rView.setAdapter(cardAdapter);
+        bottomNavigationView = findViewById(R.id.bottom_menu);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        Toolbar topmenu = findViewById(R.id.top_menu);
+        setSupportActionBar(topmenu);
+
+        Menu menu = topmenu.getMenu();
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        topmenu.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.top_menu) {
+                    // Handle search action
+                    return true;
+                }
+                // Handle other menu item clicks if needed
+                return false;
+            }
+        });
+
 
     }
 
@@ -35,5 +63,26 @@ public class SearchView extends AppCompatActivity {
 
 
         return products;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.search) {
+            // Handle search menu item click
+            // Example: startActivity(new Intent(SearchView.this, SearchActivity.class));
+            return true;
+        } else if (item.getItemId() == R.id.category) {
+            // Handle profile menu item click
+            // Example: startActivity(new Intent(SearchView.this, ProfileActivity.class));
+            return true;
+        }
+        return false;
     }
 }
