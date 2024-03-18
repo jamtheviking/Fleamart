54a59fe9457dcb4f13e1b78a8ebb58700317732b
Kareem Katiya <130093787+HoneyB2020@users.noreply.github.com>
2024-03-18 14:13:32 -0700

    search bar working
    implement searchItem method
    created customer edit text to allows for diffenrent listeners

--- app/src/main/java/com/csis3175/fleamart/features/SearchView.java
+++ app/src/main/java/com/csis3175/fleamart/features/SearchView.java
@@ -1,17 +1,19 @@
 package com.csis3175.fleamart.features;
 
 import android.os.Bundle;
-
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.csis3175.fleamart.model.CardAdapter;
+import com.csis3175.fleamart.model.CustomEditText;
 import com.csis3175.fleamart.model.Product;
 import com.csis3175.fleamart.R;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SearchView extends AppCompatActivity {
 
@@ -33,7 +35,49 @@ public class SearchView extends AppCompatActivity {
 
         rView.setLayoutManager(gridLayoutManager);
         CardAdapter cardAdapter = new CardAdapter(getCardData());
+
         rView.setAdapter(cardAdapter);
+
+        CustomEditText searchBar = findViewById(R.id.search_bar);
+
+        //Override Perform click with CustomEditText and configure OnTouch for right side of edit box and for Enter Key
+        searchBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // drawableEndPosition = right side of edit text with Icon drawable
+                int drawableEndPosition = searchBar.getRight() - searchBar.getCompoundDrawables()[2].getBounds().width();
+                String searchTerm = Objects.requireNonNull(searchBar.getText()).toString().toLowerCase();
+                if (event.getAction() == MotionEvent.ACTION_UP && event.getRawX() >= drawableEndPosition) {
+                    searchItems(searchTerm, cardAdapter, rView);
+                    return true;
+                }
+                return false;
+            }
+        });
+        searchBar.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    String searchTerm = Objects.requireNonNull(searchBar.getText()).toString().toLowerCase();
+                    searchItems(searchTerm,cardAdapter,rView);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void searchItems(String searchTerm, CardAdapter ca,RecyclerView rView){
+
+        List<Product> allProducts = new ArrayList<>(getCardData());
+        List<Product> updatedList = new ArrayList<>();
+        for(Product product : allProducts){
+            if (product.getItemName().toLowerCase().contains(searchTerm)) {
+                updatedList.add(product);
+            }
+        }
+        ca.updatedList(updatedList);
+        rView.setAdapter(ca);
 
     }
 
@@ -53,7 +97,6 @@ public class SearchView extends AppCompatActivity {
         products.add(new Product("Coffee Maker",25,R.drawable.coffee1));
         products.add(new Product("Coffee Maker2",20,R.drawable.coffee2));
         products.add(new Product("Dryer Machine",12000,R.drawable.dryer));
-
 
         return products;
     }
